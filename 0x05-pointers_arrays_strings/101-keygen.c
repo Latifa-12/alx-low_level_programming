7f4b33226bc1da227631daa4c20633707d162e6d
Latifa-12 <125558460+Latifa-12@users.noreply.github.com>
2023-03-27 22:05:21 +0200

    Create 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 8
+
+/**
+ * C that generates random passwords
+ *
+ * @s: This program generates a random password with a length of PASSWORD_LENGTH characters
+ *
+ * Return: Succes
+*/
+
+int main() {
+    char password[PASSWORD_LENGTH + 1];
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    int i;
+
+    srand(time(NULL));
+
+    for (i = 0; i < PASSWORD_LENGTH; i++) {
+        password[i] = charset[rand() % (sizeof(charset) - 1)];
+    }
+    password[PASSWORD_LENGTH] = '\0';
+
+    printf("Tada! Congrats: %s\n", password);
+
+    return 0;
+}
