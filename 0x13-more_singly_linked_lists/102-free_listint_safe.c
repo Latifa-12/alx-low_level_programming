bd2c52680cf4a9958f619d31293e5170a2761057
Latifa-12 <125558460+Latifa-12@users.noreply.github.com>
2023-05-03 02:02:54 +0300

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,1 +1,32 @@
+#include "lists.h"
 
+/**
+ * free_listint_safe - frees a listint_t list.
+ * @h: pointer to the head of the list
+ * Return: the size of the list that was free'd
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	size_t count = 0;
+	listint_t *current, *tmp;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	current = *h;
+
+	while (current != NULL)
+	{
+		count++;
+		tmp = current;
+		current = current->next;
+		free(tmp);
+
+		if (tmp < current)
+			break;
+	}
+	*h = NULL;
+
+	return (count);
+}
